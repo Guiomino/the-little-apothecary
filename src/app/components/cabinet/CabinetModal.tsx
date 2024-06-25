b958392a9176797ea479d0b55591448ded346378
GuillaumeSimplon <estrade.guillaume.simplon@gmail.com>
2024-06-25 12:09:55 +0200

    feat(CabinetModal)

--- src/app/components/cabinet/CabinetModal.tsx
+++ src/app/components/cabinet/CabinetModal.tsx
@@ -1,8 +1,18 @@
 // CabinetModal.tsx
 
-import React from "react";
+"use client"
+
+import React, { useEffect, useRef, useState } from "react";
+import CloseModal from '@/app/components/miscellaneous/CloseModal';
 import IngredientsListCabinet from "./IngredientsListCabinet";
 import styles from "./cabinetModal.module.scss";
+import LevelUser from "../miscellaneous/LevelUser";
+import GoldCoins from "../miscellaneous/GoldCoins";
+import FilterIngredients from "../miscellaneous/FilterIngredients";
+import UserClass from '@/app/OOP/UserClass';
+import AppButton from "../miscellaneous/AppButton";
+import Image from "next/image";
+
 
 export interface Ingredient {
     ingredient: {
@@ -23,13 +33,73 @@ interface CabinetModalProps {
 }
 
 const CabinetModal: React.FC<CabinetModalProps> = ({ ingredients, onCloseClick }) => {
+
+    const userRef = useRef(UserClass.loadFromLocalStorage() || new UserClass([], "Guest"));
+    const user = userRef.current;
+
+    const [showFilter, setShowFilter] = useState<boolean>(false);
+    const [goldCoins, setGoldCoins] = useState(user.gold);
+    const [isBuying, setIsBuying] = useState(false);
+
+    useEffect(() => {
+        user.gold = goldCoins;
+        user.saveToLocalStorage();
+    }, [goldCoins, user]);
+
+    const handleFilterClick = () => {
+        setShowFilter(true);
+    };
+
+    const handleBuy = () => {
+        return <p></p>
+    }
+
     return (
-        <div className={styles.modalOverlay}>
-            <div className={styles.modal}>
-                <button className={styles.closeButton} onClick={onCloseClick}>X</button>
-                <IngredientsListCabinet ingredients={ingredients} />
+        <section className={styles.cabinetOverlay}>
+            <div className={styles.cabinetModal}>
+
+                <div className={styles.top}>
+                    <FilterIngredients onFilterClick={handleFilterClick} />
+                    <GoldCoins initialGoldCoins={goldCoins} />
+                    <h2>Cabinet</h2>
+                    <LevelUser />
+                    <CloseModal onClick={onCloseClick} />
+                </div>
+
+
+                <div className={`${styles.list} ${styles.filter} ${styles.details}`}>
+                    <IngredientsListCabinet ingredients={ingredients} />
+                </div>
+
+
+                <div className={styles.summary}>
+                    <p><strong>Starting price : </strong><span>95</span></p>
+                    <h2>Selection</h2>
+                    <p><strong>Selling price : </strong><span>115</span></p>
+                </div>
+
+                <div className={styles.cartList}>
+                    List of 2 ingredients
+                </div>
+
+
+                <div className={styles.chanceOfProfit}>
+                    <p><strong>Profit : </strong><span>20</span></p>
+                    <p><strong>Success rate : </strong><span>72%</span></p>
+                </div>
+
+
+                <div className={styles.cartListBtn}>
+                    <AppButton
+                        label={"🍶 Choose"}
+                        onClick={handleBuy}
+                        disabled={isBuying}
+                    />
+                </div>
+
+
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/app/components/miscellaneous/AppButtonBranch.tsx
+++ src/app/components/miscellaneous/AppButtonBranch.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import styles from "./miscellaneous.module.scss"
+
 const AppButtonBranch = () => {
     return (
         <div className={styles.AppButtonBranch}>
