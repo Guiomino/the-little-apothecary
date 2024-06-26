2425cc50bf1b06bb5b98ae513915bbfb93898173
GuillaumeSimplon <estrade.guillaume.simplon@gmail.com>
2024-06-26 15:39:54 +0200

    styles(IngredientsListCabinet)

--- src/app/components/cabinet/IngredientsListCabinet.tsx
+++ src/app/components/cabinet/IngredientsListCabinet.tsx
@@ -28,20 +28,59 @@ const IngredientsListCabinet: React.FC<IngredientsListCabinetProps> = ({ ingredi
         setCurrentIngredients(ingredients);
     }, [ingredients]);
 
+    const getRarityClass = (rarity: string) => {
+        switch (rarity) {
+            case 'Common':
+                return styles.common;
+            case 'Uncommon':
+                return styles.uncommon;
+            case 'Rare':
+                return styles.rare;
+            case 'Epic':
+                return styles.epic;
+            default:
+                return '';
+        }
+    };
+
+
     return (
-        <ul>
-            {currentIngredients.map((ingredient, index) => (
-                <li key={index}>
-                    <Image src={ingredient.ingredient.imagePath} width={40} height={40} alt={ingredient.ingredient.name} />
-                    <h3>{ingredient.ingredient.name}</h3>
-                    <p>Type: {ingredient.ingredient.type}</p>
-                    <p>Rarity: {ingredient.ingredient.rarity}</p>
-                    <p>{ingredient.ingredient.description}</p>
-                    <p>Price: {ingredient.ingredient.price}</p>
-                    <p>Success rate: {ingredient.ingredient.successRate}%</p>
-                    <p>Quantity: {ingredient.quantity}</p>
-                </li>
-            ))}
+        <ul className={styles.ingredientsContainer}>
+            {currentIngredients.map((ingredient, index) => {
+                const rarityClass = getRarityClass(ingredient.ingredient.rarity);
+
+                return (
+                    <li key={index} className={`${styles.ingredient} ${rarityClass}`}>
+
+                        <div className={styles.ingrImage}>
+                            <Image src={ingredient.ingredient.imagePath} width={40} height={40} alt={ingredient.ingredient.name} />
+                        </div>
+
+                        <div className={styles.ingrTitle}>
+                            <button className={styles.buttonWithOverlay}>{ingredient.ingredient.name}</button>
+                            <div className={styles.ingrTypeAndRarity}>
+                                <p>Type: {ingredient.ingredient.type}</p>
+                                <p>Rarity: {ingredient.ingredient.rarity}</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.ingrHandle}>
+                            <div className={styles.price}>
+                                <p>Price: {ingredient.ingredient.price}</p>
+                            </div>
+
+                            <div className={styles.successRate}>
+                                <p>Success rate: {ingredient.ingredient.successRate}%</p>
+                            </div>
+
+                            <div className={styles.quantity}>
+                                <p>Quantity: {ingredient.quantity}</p>
+                            </div>
+                        </div>
+                        <button className={styles.add}>Add</button>
+                    </li>
+                )
+            })}
         </ul>
     );
 };
